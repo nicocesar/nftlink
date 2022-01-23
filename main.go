2d06744c415fd5abebb839ccb148e53a45851637
Nico Cesar <nico@nicocesar.com>
2022-01-23 12:16:59 -0500

    added /check endpoint

--- main.go
+++ main.go
@@ -51,11 +51,13 @@ func (worker *worker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	retrievedVal := &ClaimPrize{}
 	found, err := worker.store.Get(key, &retrievedVal)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%v", err)
+		return
 	}
 	if !found {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "Key %s not found", key)
+		fmt.Fprintf(w, "Redeem code %s not found", key)
 		return
 	}
 
@@ -187,6 +189,31 @@ func RandomString(n int) string {
 	return string(s)
 }
 
+type checker struct {
+	store gokv.Store
+}
+
+func (worker *checker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	retrievedVal := &ClaimPrize{}
+	found, err := worker.store.Get(key, &retrievedVal)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Redeem code %s not found", key)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Redeem code %s found", key)
+}
+
 var initFlag = flag.Bool("init", false, "initialize the database")
 
 func main() {
@@ -216,6 +243,9 @@ func main() {
 
 	minter := &worker{store: store}
 	r.Handle("/mint/{id}/{wallet}", minter)
+
+	checker := &checker{store: store}
+	r.Handle("/check/{id}", checker)
 
 	// Add some profiling.
 	r.Handle("/debug/pprof/profile", http.DefaultServeMux)
